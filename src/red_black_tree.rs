a54e87252d8d855c3c5b5a8b5cf5023d373c6554
Fahrin Bushra <87880319+Fahrin0802@users.noreply.github.com>
2024-03-17 00:20:06 -0400

    Update red_black_tree.rs

--- src/red_black_tree.rs
+++ src/red_black_tree.rs
@@ -1,9 +1,45 @@
+// use crate::tree::*;
+
+// pub struct RedBlackNode {
+//     color: String //change to your needs
+// }
+
+// pub struct RedBlackTree<T> {
+//     root: TreeNode<T>
+// }
+
+use std::cell::RefCell;
+use std::rc::Rc;
 use crate::tree::*;
 
-pub struct RedBlackNode {
-    color: String //change to your needs
+#[derive(Debug)]
+enum NodeColor {
+    Red,
+    Black
 }
 
-pub struct RedBlackTree<T> {
-    root: TreeNode<T>
+#[derive(Debug)]
+pub struct RedBlackNode {
+    color: NodeColor
+}
+
+
+impl<T: Ord + Clone> TreeNode<T> {
+    pub fn new_red_black(key: T) -> TreeNode<T> {
+        TreeNode {
+            key: key,
+            left_child: None,
+            right_child: None,
+            parent: None,
+            kind: Node::RedBlack(RedBlackNode{color: NodeColor::Red}),
+        }
+    }
+
+    pub fn recolor_red_black(&mut self) {
+
+    }
+
+    pub fn red_black_insert_node(&mut self, key: T) {
+        self.binary_tree_insert(key)
+    }
 }
