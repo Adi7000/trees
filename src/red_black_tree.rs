5abfc21f80473184bc8ceab3872a3b9d925aadef
Fahrin Bushra <87880319+Fahrin0802@users.noreply.github.com>
2024-03-17 08:46:23 -0400

    borrowing problems in recolor.

--- src/red_black_tree.rs
+++ src/red_black_tree.rs
@@ -1,4 +1,7 @@
-// use crate::tree::*;
+use crate::tree::*;
+use std::cell::RefCell;
+use std::rc::Rc;
+use std::rc::Weak;
 
 // pub struct RedBlackNode {
 //     color: String //change to your needs
@@ -8,32 +11,80 @@
 //     root: TreeNode<T>
 // }
 
-use crate::tree::*;
+// #[derive(Debug)]
+// pub struct RedBlackNode {
+//     color: NodeColor
+// }
 
-#[derive(Debug)]
+
+// impl<T: Ord + Clone> TreeNode<T> {
+// }
+
+#[derive(Clone, Debug, PartialEq)]
 enum NodeColor {
     Red,
-    Black
+    Black,
 }
+type RedBlackTree = Option<Rc<RefCell<TreeNode>>>;
 
 #[derive(Debug)]
-pub struct RedBlackNode {
-    color: NodeColor
+struct TreeNode {
+    pub color: NodeColor,
+    pub key: u32,
+    pub parent: RedBlackTree,
+    left: RedBlackTree,
+    right: RedBlackTree,
 }
 
 
-impl<T: Ord + Clone> TreeNode<T> {
-    pub fn new_red_black(key: T) -> TreeNode<T> {
-        TreeNode {
-            key: key,
-            left_child: None,
-            right_child: None,
-            parent: None,
-            kind: Node::RedBlack(RedBlackNode{color: NodeColor::Red}),
-        }
+pub fn new(key: u32) -> RedBlackTree{
+    let bushra:TreeNode = TreeNode{
+        color: NodeColor::Red,
+        key: key,
+        parent: None,
+        left: None,
+        right: None,
+    };
+    Some(Rc::new(RefCell::new(bushra)))
+}
+
+pub fn insert_node(rb_tree: &mut RedBlackTree, key: u32) {
+    let rcnode = rb_tree.as_ref().unwrap();
+    let mut current_node = rcnode.borrow_mut();
+    
+    //prevent duplicate insertion
+    if current_node.key == key {
+        return;
     }
 
-    pub fn red_black_insert_node(&mut self, key: T) {
-        self.binary_tree_insert(key)
+    let temp = if key < current_node.key {
+        &mut current_node.left
+    } else {
+        &mut current_node.right
+    };
+
+    match temp {
+        Some(_) => {
+            insert_node(temp, key);
+        }
+        None => {
+
+            let newnode:TreeNode = TreeNode{
+                color: NodeColor::Red,
+                key: key,
+                parent: Some(Rc::clone(rcnode)),
+                left: None,
+                right: None,
+            }; 
+
+            let newnode = Rc::new(RefCell::new(newnode));
+            if key < current_node.key {
+                current_node.left = Some(newnode.clone());
+                // recolor(&mut current_node.left);
+            } else {
+                current_node.right = Some(newnode.clone());
+                // recolor(&mut current_node.right);
+            };
+        }
     }
 }
