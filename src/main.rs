0a163a4168ced7668aebacbfb3abaadbe30247c3
Rahul <rtabraha@ualberta.ca>
2024-03-20 13:56:56 -0600

    add colors to print

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ use std::env;
 
 fn main() {
     env::set_var("RUST_BACKTRACE", "1");
-    let mut rbt = avl_tree::AvlTree::new();
+    let mut avl = avl_tree::AvlTree::new();
+    let mut rbt = red_black_tree::RedBlackTree::new();
 
     rbt.insert(3);
     rbt.insert(2);
@@ -13,8 +14,17 @@ fn main() {
     rbt.insert(4);
     rbt.insert(5);
 
-    
+    avl.insert(3);
+    avl.insert(2);
+    avl.insert(1);
+    avl.insert(4);
+    avl.insert(5);
+
+    avl.print_inorder();
+    println!("Height of AVL tree is {}", avl.height());
+    avl.print_tree();
+
     rbt.print_inorder();
-    println!("Height of RBT tree is {}", rbt.height());
+    println!("Height of Red Black tree is {}", rbt.height());
     rbt.print_tree();
 }
